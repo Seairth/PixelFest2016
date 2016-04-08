977d07fa9029ed3116a8acec5b822f588b9b80bb
Scott Rising <scott@xerxes3rd.net>
2016-04-08 14:55:03 -0400

    Fixed network rotation handling. Added BULLETS!!!!!!!!!!!!!111one

--- Misc/OctoSwim/Assets/Scripts/OctoMove.cs
+++ Misc/OctoSwim/Assets/Scripts/OctoMove.cs
@@ -5,11 +5,14 @@ using UnityEngine.Networking;
 public class OctoMove : NetworkBehaviour {
 
     private Rigidbody2D body;
-    [SyncVar(hook = "UpdateRot")]
-    public float SpriteRot;
+    //[SyncVar(hook = "UpdateRot")]
+    //public float SpriteRot;
 
     public float force = 0;
     public float turnRate = 0;
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
 
 	// Use this for initialization
 	void Start () {
@@ -25,7 +28,7 @@ public class OctoMove : NetworkBehaviour {
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
         if (isLocalPlayer)
         {
             Control();   
@@ -42,31 +45,50 @@ public class OctoMove : NetworkBehaviour {
             //SpriteRot = ;
             //CmdSetSpriteRot(new Vector3(0, 0, turnRate));
             body.rotation += turnRate;
-            CmdSetSpriteRot(body.rotation);
+            //CmdSetSpriteRot(body.rotation);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             //SpriteRot = ;
             //CmdSetSpriteRot(new Vector3(0, 0, -turnRate));
             body.rotation -= turnRate;
-            CmdSetSpriteRot(body.rotation);
+            //CmdSetSpriteRot(body.rotation);
         }
-        else
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
+            Fire();
+            //body.
             //SpriteRot = new Vector3(0, 0, 0);
         }
     }
 
-    [Command]
+    void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        var b = bullet.GetComponent<Rigidbody2D>();
+        b.velocity = bullet.transform.up * 6;
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+
+    //[Command]
     void CmdSetSpriteRot(float rot)
     {
-        SpriteRot = rot;
+        //SpriteRot = rot;
     }
 
     void UpdateRot(float rot)
     {
         //transform.Rotate(SpriteRot);
-        body.rotation = rot;
+        //body.rotation = rot;
         //Debug.Log(string.Format("newpos: x: {0} y: {1} z: {2}", newpos.x, newpos.y, newpos.z));
     }
 }
