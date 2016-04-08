f0cd92c22fcf3f0f06e2649381f679b43c897cad
Scott Rising <scott@xerxes3rd.net>
2016-04-08 12:23:06 -0400

    Added networking.

--- Misc/OctoSwim/Assets/Scripts/OctoMove.cs
+++ Misc/OctoSwim/Assets/Scripts/OctoMove.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class OctoMove : MonoBehaviour {
+public class OctoMove : NetworkBehaviour {
 
     private Rigidbody2D body;
+    [SyncVar(hook = "UpdateRot")]
+    public float SpriteRot;
 
     public float force = 0;
     public float turnRate = 0;
@@ -11,17 +14,59 @@ public class OctoMove : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         body = GetComponent<Rigidbody2D>();
+
+        if (isLocalPlayer)
+        {
+            foreach (var src in GetComponents<AudioSource>())
+            {
+                src.Play();
+            }
+        }
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        if (isLocalPlayer)
+        {
+            Control();   
+        }
+    }
+
+    void Control()
+    {
         if (Input.GetKey(KeyCode.UpArrow))
             body.AddForce(transform.up * force);
 
         if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Rotate(new Vector3(0, 0, turnRate));
+        {
+            //SpriteRot = ;
+            //CmdSetSpriteRot(new Vector3(0, 0, turnRate));
+            body.rotation += turnRate;
+            CmdSetSpriteRot(body.rotation);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //SpriteRot = ;
+            //CmdSetSpriteRot(new Vector3(0, 0, -turnRate));
+            body.rotation -= turnRate;
+            CmdSetSpriteRot(body.rotation);
+        }
+        else
+        {
+            //SpriteRot = new Vector3(0, 0, 0);
+        }
+    }
 
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.Rotate(new Vector3(0, 0, -turnRate));
+    [Command]
+    void CmdSetSpriteRot(float rot)
+    {
+        SpriteRot = rot;
+    }
+
+    void UpdateRot(float rot)
+    {
+        //transform.Rotate(SpriteRot);
+        body.rotation = rot;
+        //Debug.Log(string.Format("newpos: x: {0} y: {1} z: {2}", newpos.x, newpos.y, newpos.z));
     }
 }
