b1160270869e6f9db9a101ce1cd05d49af0bf822
seairth <seairth@gmail.com>
2016-04-08 10:01:49 -0400

    Initial snapshot
    Basic 2D scene with minimal sprite animation, arrow key control, and
    background music.

---
+++ Misc/OctoSwim/Assets/Scripts/OctoMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OctoMove : MonoBehaviour {
+
+    private Rigidbody2D body;
+
+    public float force = 0;
+    public float turnRate = 0;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.UpArrow))
+            body.AddForce(transform.up * force);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(new Vector3(0, 0, turnRate));
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(new Vector3(0, 0, -turnRate));
+    }
+}
