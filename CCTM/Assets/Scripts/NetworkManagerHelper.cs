8aa1e8b82be95fa5720cf5fc323256c8009131bb
Scott Rising <scott@xerxes3rd.net>
2016-04-09 10:39:24 -0400

    Added missing script.

---
+++ CCTM/Assets/Scripts/NetworkManagerHelper.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine.SceneManagement;
+
+public class NetworkManagerHelper : NetworkManager {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+}
