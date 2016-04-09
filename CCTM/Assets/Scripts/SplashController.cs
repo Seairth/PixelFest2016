c701e00e393df3c3b904c1effe3223a2c77b9466
seairth <seairth@gmail.com>
2016-04-09 11:53:47 -0400

    Splash Screen start

---
+++ CCTM/Assets/Scripts/SplashController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class SplashController : MonoBehaviour {
+
+    private NetworkManager manager;
+    
+    public Button startHostButton;
+    public Button startClientButton;
+    
+    void Awake()
+    {
+        manager = FindObjectOfType<NetworkManager>();
+    }
+    
+    public void StartHost()
+    {
+        manager.StartHost();
+    }
+    
+    public void StartClient()
+    {
+        manager.StartClient();
+    }
+}
