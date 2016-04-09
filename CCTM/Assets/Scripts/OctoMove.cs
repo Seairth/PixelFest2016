40372a5e1aa875ac0be5ca76f0e89072b6eafa9d
Scott Rising <scott@xerxes3rd.net>
2016-04-09 09:34:39 -0400

    Added in sample player assets and scripts.

---
+++ CCTM/Assets/Scripts/OctoMove.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class OctoMove : NetworkBehaviour {
+
+    private Rigidbody2D body;
+    //[SyncVar(hook = "UpdateRot")]
+    //public float SpriteRot;
+
+    public float force = 0;
+    public float turnRate = 0;
+
+    //public GameObject bulletPrefab;
+    //public Transform bulletSpawn;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+
+        if (isLocalPlayer)
+        {
+            foreach (var src in GetComponents<AudioSource>())
+            {
+                src.Play();
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isLocalPlayer)
+        {
+            Control();   
+        }
+    }
+
+    void Control()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            body.AddForce(transform.up * force);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            body.AddTorque(turnRate);
+            //body.rotation = lastRotation + turnRate;
+            //lastRotation = body.rotation;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            body.AddTorque(-turnRate);
+            //body.rotation = lastRotation - turnRate;
+            //lastRotation = body.rotation;
+        }
+        else
+        {
+            //if (body.rotation != lastRotation)
+            //    body.rotation = lastRotation;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        /*
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        var b = bullet.GetComponent<Rigidbody2D>();
+        b.velocity = bullet.transform.up * 6;
+
+        NetworkServer.Spawn(bullet);
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+        */
+    }
+
+    //[Command]
+    void CmdSetSpriteRot(float rot)
+    {
+        //SpriteRot = rot;
+    }
+
+    void UpdateRot(float rot)
+    {
+        //transform.Rotate(SpriteRot);
+        //body.rotation = rot;
+        //Debug.Log(string.Format("newpos: x: {0} y: {1} z: {2}", newpos.x, newpos.y, newpos.z));
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        //Debug.Log("Player collide");
+    }
+}
---
+++ CCTM/Assets/Scripts/OctoSound.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OctoSound : MonoBehaviour {
+    public AudioMixerSnapshot ambient;
+    public AudioMixerSnapshot combat;
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Combat"))
+        {
+            combat.TransitionTo(0);
+        }
+    }
+    
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Combat"))
+        {
+            ambient.TransitionTo(0);
+        }        
+    }
+}
