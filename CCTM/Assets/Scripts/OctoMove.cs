65d01f60e09ea89f204c34c26abf88cc6dc454c3
Scott Rising <scott@xerxes3rd.net>
2016-04-09 11:47:14 -0400

    Added sample walls.

--- CCTM/Assets/Scripts/OctoMove.cs
+++ CCTM/Assets/Scripts/OctoMove.cs
@@ -41,22 +41,8 @@ public class OctoMove : NetworkBehaviour {
         if (isLocalPlayer)
         {
             Control();
-            CameraFollow(body.transform);
         }
     }
-    
-    void CameraFollow(Transform target)
-    {
-        if (target)
-        {
-            var camera = GetComponent<Camera>();
-            Vector3 point = camera.WorldToViewportPoint(target.position);
-            Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
-            Vector3 destination = transform.position + delta;
-            transform.position = Vector3.SmoothDamp(transform.position, destination, ref camVelocity, camDampTime);
-        }
-    }
-    
 
     void Control()
     {
