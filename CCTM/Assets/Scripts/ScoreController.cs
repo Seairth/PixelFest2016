7b0cfca385ce7c765c5ea32b866cc42ffeb5dc96
seairth <seairth@gmail.com>
2016-04-10 15:00:18 -0400

    Added pointless stuff

---
+++ CCTM/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour {
+	
+
+	void OnClientConnected(NetworkPlayer player)
+	{
+		Debug.Log("Client Connected");
+	}
+}
