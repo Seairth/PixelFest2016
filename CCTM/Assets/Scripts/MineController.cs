9d42b84accc1e1cea759bfe4321d5c84c73e901c
seairth <seairth@gmail.com>
2016-04-10 13:43:22 -0400

    Animating mine!

---
+++ CCTM/Assets/Scripts/MineController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MineController : MonoBehaviour {
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			var mover = GetComponent<EnemyMover>();
+
+			mover.HorizontalSpeed = 0;
+			mover.Speed = 0;
+
+			var anim = GetComponent<Animator>();
+			anim.SetTrigger("Explode");
+
+			Destroy(gameObject, 2);
+		}
+	}
+}
